3dbee6c43f2e98d8d38df03fe54214aa330531ca
MyNameMatters <ellistanner.3@gmail.com>
2022-06-04 20:20:12 -0600

    added js variables for adding an item. Edit and delete broken

---
+++ main.js
@@ -0,0 +1,61 @@
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const listElement = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const task = input.value;
+    if (!task) {
+      alert("Please add a task");
+      return;
+    } else {
+      const taskItem = document.createElement("div");
+      taskItem.classList.add("task");
+      
+      const taskContent = document.createElement("div");
+      taskContent.classList.add("content");
+      
+      taskItem.appendChild(taskContent);
+      
+      const taskInput = document.createElement("input");
+      taskInput.classList.add("text");
+      taskInput.type = "text";
+      taskInput.value = task;
+      taskInput.setAttribute("readonly", "readonly")
+     
+      taskContent.appendChild(taskInput);
+
+      const taskActions = document.createElement("div");
+      taskActions.classList.add("actions")
+
+      const taskEdit = document.createElement("button");
+      taskEdit.classList.add("edit");
+      taskEdit.innerHTML = "Edit";
+
+      const taskDelete = document.createElement("button");
+      taskDelete.classList.add("delete");
+      taskDelete.innerHTML = "Delete";
+
+      taskActions.appendChild(taskEdit);
+      taskActions.appendChild(taskDelete);
+
+      taskItem.appendChild(taskActions);
+
+      listElement.appendChild(taskItem);
+
+      input.value = "";
+
+      taskEdit.addEventListener('click', () => {
+          if (taskEdit.innerText.toLowerCase == "edit") {
+            taskInput.removeAttribute("readonly")
+            taskInput.focus();
+            taskEdit.innerText = "Save"
+          } else {
+              taskInput.setAttribute("readonly", "readonly")
+              taskInput.innerText = "Edit"
+          }
+      })
+    }
+  });
+});
