3e65ed9f0d324df841dac451ea83841b348b866c
MyNameMatters <ellistanner.3@gmail.com>
2022-06-05 21:03:30 -0600

    fixing a few things

--- main.js
+++ main.js
@@ -1,61 +1,67 @@
-window.addEventListener("load", () => {
-  const form = document.querySelector("#new-task-form");
-  const input = document.querySelector("#new-task-input");
-  const listElement = document.querySelector("#tasks");
+window.addEventListener('load', () => {
+	const form = document.querySelector("#new-task-form");
+	const input = document.querySelector("#new-task-input");
+	const listElement = document.querySelector("#tasks");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const task = input.value;
-    if (!task) {
-      alert("Please add a task");
-      return;
-    } else {
-      const taskItem = document.createElement("div");
-      taskItem.classList.add("task");
-      
-      const taskContent = document.createElement("div");
-      taskContent.classList.add("content");
-      
-      taskItem.appendChild(taskContent);
-      
-      const taskInput = document.createElement("input");
-      taskInput.classList.add("text");
-      taskInput.type = "text";
-      taskInput.value = task;
-      taskInput.setAttribute("readonly", "readonly")
-     
-      taskContent.appendChild(taskInput);
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
 
-      const taskActions = document.createElement("div");
-      taskActions.classList.add("actions")
+		const task = input.value;
 
-      const taskEdit = document.createElement("button");
-      taskEdit.classList.add("edit");
-      taskEdit.innerHTML = "Edit";
+        if(!task) {
+            alert('Please add a task')
+            return;
+        }
 
-      const taskDelete = document.createElement("button");
-      taskDelete.classList.add("delete");
-      taskDelete.innerHTML = "Delete";
+		const taskItem = document.createElement('div');
+		taskItem.classList.add('task');
 
-      taskActions.appendChild(taskEdit);
-      taskActions.appendChild(taskDelete);
+		const taskContent = document.createElement('div');
+		taskContent.classList.add('content');
 
-      taskItem.appendChild(taskActions);
+		taskItem.appendChild(taskContent);
 
-      listElement.appendChild(taskItem);
+		const taskInput = document.createElement('input');
+		taskInput.classList.add('text');
+		taskInput.type = 'text';
+		taskInput.value = task;
+		taskInput.setAttribute('readonly', 'readonly');
 
-      input.value = "";
+		taskContent.appendChild(taskInput);
 
-      taskEdit.addEventListener('click', () => {
-          if (taskEdit.innerText.toLowerCase == "edit") {
-            taskInput.removeAttribute("readonly")
-            taskInput.focus();
-            taskEdit.innerText = "Save"
-          } else {
-              taskInput.setAttribute("readonly", "readonly")
-              taskInput.innerText = "Edit"
-          }
-      })
-    }
-  });
+		const taskActions = document.createElement('div');
+		taskActions.classList.add('actions');
+
+		const taskEdit = document.createElement('button');
+		taskEdit.classList.add('edit');
+		taskEdit.innerText = 'Edit';
+
+		const taskDelete = document.createElement('button');
+		taskDelete.classList.add('delete');
+		taskDelete.innerText = 'Delete';
+
+		taskActions.appendChild(taskEdit);
+		taskActions.appendChild(taskDelete);
+
+		taskItem.appendChild(taskActions);
+
+		listElement.appendChild(taskItem);
+
+		input.value = '';
+
+		taskEdit.addEventListener('click', (e) => {
+			if (taskEdit.innerText.toLowerCase() == "edit") {
+				taskEdit.innerText = "Save";
+				taskInput.removeAttribute("readonly");
+				taskInput.focus();
+			} else {
+				taskEdit.innerText = "Edit";
+				taskInput.setAttribute("readonly", "readonly");
+			}
+		});
+
+		taskDelete.addEventListener('click', (e) => {
+			listElement.removeChild(taskItem);
+		});
+	});
 });
